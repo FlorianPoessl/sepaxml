cbf0842437bc02cc8151011757a4dc7919cabb34
Florian Pössl <florian.poessl@faw.at>
2023-01-09 17:05:26 +0100

    Initial commit

---
+++ src/main/java/at/poessl/sepaxml/SepaxmlApplication.java
@@ -0,0 +1,13 @@
+package at.poessl.sepaxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepaxmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SepaxmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/poessl/sepaxml/SepaxmlApplicationTests.java
@@ -0,0 +1,13 @@
+package at.poessl.sepaxml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepaxmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
